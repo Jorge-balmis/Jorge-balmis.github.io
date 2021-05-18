bd40a3c48cd2065fced3d62fe4acd3204ced12bf
Jorge Llopis <jorge.llopis@iesdoctorbalmis.com>
2021-05-18 09:24:55 +0200

    Añado la funcion gestionarFicheroTXT a mi js

--- Ejercicio2/Ejericio2.js
+++ Ejercicio2/Ejericio2.js
@@ -1,1 +1,20 @@
 'use strict';
+loadDocA("mensaje.txt","txt");
+
+
+function gestionarFicheroTXT(cadena){
+    let mensajes = new Array;
+    let mensajes = cadena.split("\n");
+    let body = document.querySelector("body");
+    let fila = "";
+
+    for(let i = 0; i < mensajes.length; i++){
+        if(i%2 == 0){
+            fila += "<div class = 'TextoEmisor'>" + mensajes[i] + "</div>"; 
+        }
+        else{
+            fila += "<div class = 'TextoReceptor'>" + mensajes[i] + "</div>"; 
+        }
+    }
+    body.innerHTML = fila;
+}
