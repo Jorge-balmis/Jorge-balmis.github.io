700f7ed7a0a7578dbe30ef7373899ded0825a1e1
Jorge-balmis <jorge.llopis@iesdoctorbalmis.com>
2021-05-19 21:12:05 +0200

    Prueba if de contraseña

--- Ejercicio4/Ejercicio4.js
+++ Ejercicio4/Ejercicio4.js
@@ -7,7 +7,7 @@ function ComprobarCampos(){
     let capaErrores = document.getElementsByClassName(".errores")
 
     if(!usuario.match(/^[A-Z].*[0-9]$/)) errores += "El usuario no es correcto, tiene que ser primera mayuscula y la ultima un numero"; //No funciona pero si que entra en el if
-    if(!contrasena.match())
+    if(!contrasena.match(/^[0-9].*[A-Z]{8}$/)) alert("Contraseña no valida");
 
 
 
