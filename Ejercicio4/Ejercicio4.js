75f00f67d4029b8fdd4e8a179b28624806de9279
Jorge-balmis <jorge.llopis@iesdoctorbalmis.com>
2021-05-19 21:38:42 +0200

    vamos a ver si edad funciona

--- Ejercicio4/Ejercicio4.js
+++ Ejercicio4/Ejercicio4.js
@@ -4,13 +4,16 @@ function ComprobarCampos(){
     let usuario = document.getElementById("usuario").value;
     let contrasena = document.getElementById("contrasena").value;
     let repitaContrasena = document.getElementById("contrasenaRepetida").value;
+    let edad = document.getElementById("edad").value;
+    
     let errores = "";
     let capaErrores = document.getElementsByClassName(".errores")
+    let
 
     if(!usuario.match(/^[A-Z].*[0-9]$/)) errores += "<h1>El usuario no es correcto, tiene que ser primera mayuscula y la ultima un numero</h1>"; //No funciona pero si que entra en el if
     if(!contrasena.match(/^[0-9].*[A-Z]{8,8}$/)) errores += "Contraseña no valida"; //No funciona ni el if
-    if(contrasena != repitaContrasena) alert("errores += <h1>Las contraseñas no son iguales</h1>");
-
+    if(contrasena != repitaContrasena) errores += "<h1>Las contraseñas no son iguales</h1>";
+    if(!isNaN(edad)) alert("La edad debe ser numerica");
 
 
 
