99dfb2efdd6a684d7a99239264a14b8aae2c28a4
Jorge-balmis <jorge.llopis@iesdoctorbalmis.com>
2021-05-19 18:27:30 +0200

    a ver si asi me dice cuanto mide la contraseña

--- Ejercicio3/Ejercicio3.js
+++ Ejercicio3/Ejercicio3.js
@@ -16,7 +16,7 @@ function ComprobarCampos(Email, Contrasena){
     // console.log("Extension de email" + EstensionEmail[1]);
     if(email.includes("@iesdoctorbalmis.com")){
         alert("Tu email es del dominio del Doctor balmis");
-        alert("Longitud de contraseña: " + Contrasena.lenght);
+        alert("Longitud de contraseña: " + Contrasena.ToString().lenght);
     }
     else{
         // EtiquetaEmail.className = ".input:invalid";
