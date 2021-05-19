7445b3b81e22dd2f8343613b8e1577d7668edcbd
Jorge-balmis <jorge.llopis@iesdoctorbalmis.com>
2021-05-19 21:29:16 +0200

    Uff que asco de regexp

--- Ejercicio4/Ejercicio4.js
+++ Ejercicio4/Ejercicio4.js
@@ -3,11 +3,13 @@ document.getElementById("Boton").addEventListener("click",ComprobarCampos,false)
 function ComprobarCampos(){
     let usuario = document.getElementById("usuario").value;
     let contrasena = document.getElementById("contrasena").value;
+    let repitaContrasena = document.getElementById("contrasenaRepetida").value;
     let errores = "";
     let capaErrores = document.getElementsByClassName(".errores")
 
-    if(!usuario.match(/^[A-Z].*[0-9]$/)) errores += "El usuario no es correcto, tiene que ser primera mayuscula y la ultima un numero"; //No funciona pero si que entra en el if
-    if(!contrasena.match(/^[0-9].*[A-Z]{8,8}$/)) alert("Contraseña no valida");
+    if(!usuario.match(/^[A-Z].*[0-9]$/)) errores += "<h1>El usuario no es correcto, tiene que ser primera mayuscula y la ultima un numero</h1>"; //No funciona pero si que entra en el if
+    if(!contrasena.match(/^[0-9].*[A-Z]{8,8}$/)) alert("Contraseña no valida"); //No funciona ni el if
+    if(contrasena != repitaContrasena) errores += "<h1>Las contraseñas no son iguales</h1>";
 
 
 
