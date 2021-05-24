6bd164e6bfec7825ba5f83af741874ac24b83bac
Jorge Llopis <jorge.llopis@iesdoctorbalmis.com>
2021-05-24 10:37:48 +0200

    casi

--- Examen/Examen.js
+++ Examen/Examen.js
@@ -1,3 +1,19 @@
 'use strict';
+loadDocA("Examen.xml","xml");
+document.querySelector("input").addEventListener("mouseover",gestionarFicheroXML);
 
-
+function gestionarFicheroXML(xmlDoc){
+    // alert("Funciono");
+    let body = document.querySelector("body");
+    let series = xmlDoc.querySelectorAll("serie");
+    let nombres = xmlDoc.querySelectorAll("nombre");
+    let calificaciones = xmlDoc.querySelectorAll("calificacion");
+    for(let i = 0; i < series.length; i++){
+        if( i %2 == 0){
+            body.innerHTML += "<div class = 'color'>" + "Serie: " + nombres[i].textContent + " Calificacion: " + calificaciones[i].textContent + "</div>";
+        }
+        else{
+            body.innerHTML += "<div>" + "Serie: " + nombres[i].textContent + " Calificacion: " + calificaciones[i].textContent + "</div>";
+        }
+    }
+}
